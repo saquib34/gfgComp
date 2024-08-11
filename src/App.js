747ff3cc4482bc539632ba451588c698a1b2efb8
saquib34 <shadmanshahin6@gmail.com>
2024-08-11 21:54:35 +0530

    react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import ShapeDetection from './ShapeDetection';
+
+function App() {
+    return (
+        <div className="App">
+            <ShapeDetection />
+        </div>
+    );
+}
+
+export default App;
